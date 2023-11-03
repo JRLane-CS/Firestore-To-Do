b44ae2b2747ebc8f15bbc7470295829875993649
Jerry Lane <lan16014@byui.edu>
2023-11-03 10:07:40 -0500

    application fully functional, next is db checks

--- to_do.py
+++ to_do.py
@@ -5,70 +5,131 @@ import datetime
 
 # initialize firebase admin using private key
 key = credentials.Certificate("../serviceAccountKey.json")
-firebase_admin.initialize_app(key, {"databaseURL": "https://to-do-1c749-default-rtdb.firebaseio.com/"})
-
-# set database reference pointer
-root = db.reference("/")
-
-# get the current date and time
-datetime_object = datetime.datetime.now()
-time = datetime_object.strftime("%I:%M %p")
-date = datetime_object.strftime("%B %d, %Y")
-
-# db.reference("/").update({f"Task 5": ["Yet more new task details", f"{date}", f"{time}"]})
-task_number = 1
-# db.reference("/").update({f"Task {task_number}": ["New task details", f"{date}", f"{time}"]})
-print(f"Database contents: {root.get()}\n")
-
-# delete task 1 in the database
-# db.reference(f"/Task {task_number}").delete()
-# print(f"Database contents: {root.get()}")
-
-task_number = 2
-# db.reference("/").update({f"Task {task_number}": ["More new task details", f"{date}", f"{time}"]})
-# print(f"Database contents: {root.get()}\n")
-
-# delete task 2 in the database
-# db.reference(f"/Task {task_number}").delete()
-# print(f"Database contents: {root.get()}")
+firebase_admin.initialize_app(key, {"databaseURL": 
+    "https://to-do-1c749-default-rtdb.firebaseio.com/"})
 
 
+# function to add a task to the database
 def add_task(tasks):
-    if "Task 1" in tasks:
-        print("\nIt's here!")
+    
+    # create task prompt
+    print("\nEnter the task to be added to your to-do list:")
+    task = input()
+
+    # verify task details with user
+    print("\n\033[1;32mYou are about to create this task:\033[00m")
+    print(f"\033[1;33m{task}\033[00m")
+    user_choice = input("Is this correct (y/n)? ")
+
+    # if choice is y, add task to database
+    if user_choice.lower() == "y":
+        
+        # get unused task number for key
+        # if tasks dictionary is empty, task number is 0
+        if tasks == None:
+            task_number = 0
+
+        # otherwise, get the next empty integer key in the database
+        else:
+            task_number = 0
+            for task_number in range(len(tasks) + 1):
+                if f"Task {task_number + 1}" not in tasks:
+                    break
+            task_number += 1
+        
+        # get current date and time to insert into database
+        date = get_date()
+        time = get_time()
+        
+        # add to database 
+        db.reference("/").update(
+            {f"Task {task_number}": [f"{task}", f"{date}", f"{time}"]})
+
+        # show user what was added
+        print(f"\nOn \033[1;33m{date}\033[00m at ", end="")
+        print(f"\033[1;33m{time}\033[00m you added the task:")
+        print(f"\033[1;33m{task}\033[00m\n")
+        
+    # otherwise, return to menu without adding a task to the database
+    else:
+        print("\nQuitting add task and returning to menu...\n")
 
 
+# function to delete a task from the database
 def delete_task(task_number, tasks):
     
-    # set key name default value
-    key_name = ""
-    
-    # find key in tasks dictionary
-    for number, task in enumerate(tasks):
-        if task_number == number:
-            key_name = task
-
-    # if key name is not in database, tell user and return
-    if key_name == "":
-        print("Invalid task! Exiting delete task.")
-        return
+    # find key in tasks dictionary by calling find key function
+    key_name = find_key(task_number, tasks)
+        
+    # remember old task name
+    old_task = tasks[key_name][0]
 
     # verify deletion by user
-    print("\nVerify you want to delete this task:")
-    print(tasks[key_name][0])
-    user_choice = input(f"y/n? ")
+    print("\n\033[1;31mYou are about to delete this task:\033[00m")
+    print(f"\033[1;33m{old_task}\033[00m")
+    user_choice = input(f"Is this correct y/n? ")
+    
+    # if choice is anything other than y, return to menu
     if user_choice.lower() != "y":
-        print("\nExiting delete task.\n")
+        print("\nExiting delete task and returning to menu.\n")
         return
     
     # delete the task
     print("\nPlease wait. Deleting selected task...")
     db.reference(f"/{key_name}").delete()
-    print(f"\nTask\n{tasks[key_name][0]}\nwas deleted.\n")
+    print(f"\nTask\n\033[1;33m'{old_task}'\033[00m")
+    print(f"was deleted on \033[1;33m{get_date()}\033[00m", end=" ")
+    print(f"at \033[1;33m{get_time()}\033[00m.\n")
 
 
-def update_task(task_number, task):
-    pass
+# function to update an existing task in the database
+def update_task(task_number, tasks):
+    
+    # find key in tasks dictionary by calling find key function
+    key_name = find_key(task_number, tasks)
+    
+    # remember old task
+    old_task = tasks[key_name][0]
+
+    # verify deletion by user
+    print("\n\033[1;31mYou are about to update this task:\033[00m")
+    print(f"\033[1;33m{old_task}\033[00m")
+    user_choice = input(f"Is this correct y/n? ")
+
+    # if user didn't choose y, return to menu
+    if user_choice.lower() != "y":
+        print("Exiting update task and returning to menu...")
+        return
+
+    # create prompt
+    print("\nEnter new task information:")
+    task = input()
+
+    # verify the update with the user
+    print(f"\033[1;31mYou are about to update this task:\033[00m")
+    print(f"\033[1;33m{old_task}\033[00m")
+    print("with")
+    print(f"\033[1;33m{task}\033[00m")
+    user_choice = input(f"Is this correct y/n? ")
+
+    # if user didn't choose y, return to menu
+    if user_choice.lower() != "y":
+        print("\nExiting update task and returning to menu...\n")
+        return
+
+    # get current time and date to insert into the database
+    date = get_date()
+    time = get_time()
+
+    # update the task in the database
+    db.reference("/").update(
+            {f"{key_name}": [f"{task}", f"{date}", f"{time}"]})
+
+    # inform user of the changes
+    print(f"\nOn {date} at {time} task")
+    print(f"\033[1;33m{old_task}\033[00m")
+    print("was updated to")
+    print(f"\033[1;33m{task}\033[00m.\n")
 
 
 # print out all tasks in the database
@@ -77,16 +138,49 @@ def view_tasks(tasks):
     # print the number of tasks in database in double underlined white font
     print(f"\n\033[1;37m\033[1;21mYou have {len(tasks)} tasks:\033[00m")
     
-    # display list of tasks
+    # display list of tasks in color
     for number, task in enumerate(tasks):
-        print(f"\n\033[1;31mTask {number + 1}\033[00m")
-        print(f"\033[1;33m{tasks[task][0]}\033[00m")
-        print("set up on")
-        print(f"\033[1;33m{tasks[task][1]}\033[00m", end=" ")
-        print(f"at \033[1;33m{tasks[task][2]}\033[00m")
+        print(f"\n\033[1;31mTask {number + 1}\033[00m")       # red task header
+        print(f"\033[1;33m{tasks[task][0]}\033[00m")          # task in yellow
+        print("set up on")                                    # text in gray
+        print(f"\033[1;33m{tasks[task][1]}\033[00m", end=" ") # date in yellow
+        print(f"at \033[1;33m{tasks[task][2]}\033[00m")       # time in yellow
     
     # print clean line
     print()
+
+
+# function to find the key in the database using the user input (task number)
+def find_key(task_number, tasks):
+    
+    # find key in tasks dictionary
+    for number, task in enumerate(tasks):
+        if task_number == number:
+            return task
+        
+    print("\nTask not found in database.\n")
+
+
+# function to return current date and time
+def get_date():
+    
+    # get current date
+    datetime_object = datetime.datetime.now()
+    date = datetime_object.strftime("%B %d, %Y")
+
+    # return date and time in string format
+    return date
+
+
+#function to return current time
+def get_time():
+    
+    # get current time
+    datetime_object = datetime.datetime.now()
+    time = datetime_object.strftime("%I:%M %p")
+
+    # return time
+    return time
 
 
 # main function
@@ -98,7 +192,7 @@ def main():
     task_number = 0
 
     # welcome message
-    print("\nWelcome to Firebase To-Do Task Manager.\n")
+    print("\nWelcome to the \033[1;36mFirebase To-Do Task Manager\033[00m.\n")
     
     # to do task options loop - loop until user selects quit
     while True:
@@ -117,7 +211,7 @@ def main():
             # get user choice
             user_choice = input("\nSelect 1, 2, 3, 4, or 5: ")
             
-            # if choice is valid, break out of loop
+            # if user choice is valid, break out of loop
             if(user_choice == "1" or 
             user_choice == "2" or 
             user_choice == "3" or
@@ -125,44 +219,97 @@ def main():
             user_choice == "5"):
                 break
             
-            # if user_choice is not good, print message and loop again
+            # if user_choice is not good, inform user and loop again
             else:
                 print("\nInvalid input. Please select 1, 2, 3, 4, or 5.\n")
 
-        #get database json information and store in a dictionary
+        #set database root and create dictionary with database data
+        root = db.reference("/")
         tasks = root.get()
 
         # act on user choice
-        # TODO add a task to the to do list
+        # add a task to the to do list
         if (user_choice == "1"):
-            print("\nAdding task")
+            add_task(tasks)
 
         # delete a task from the to do list
         elif (user_choice == "2"):
+            
+            # verify there are tasks to be deleted
+            if tasks == None:
+                print("\n\033[1;31mYou have no tasks to delete.\033[00m\n")
+                continue
+
+            # list tasks in database
             view_tasks(tasks)
+
+            # loop until a valid user choice is made selecting a task to delete
             while True:
-                user_choice = input("Which task do you want to delete? ")
+                user_choice = input(
+                    "Which task number do you wish to delete? ")
+                
+                # make sure the choice can be cast to integer
                 if user_choice.isdigit():
+
+                    # cast to integer and subtract one to match actual element
                     task_number = int(user_choice) - 1
+
+                    # verify the input falls within the dictionary length
+                    # if so, call the delete task function and break loop
                     if task_number >= 0 and task_number < len(tasks):
                         delete_task(task_number, tasks)
-                        break            
+                        break 
+
+                # otherwise, alert user to invalid input and loop again               
                 print("Invalid selection, input a valid task number.")
 
         # view all tasks
         elif (user_choice == "3"):
+            
+            # if database has data, call view tasks function using the
+            # tasks dictionary holding the database data as an argument
             if tasks != None:
                 view_tasks(tasks)
+            
+            # otherwise alert user and loop again
             else:
-                print("You have no tasks to view.")
+                print("\n\033[1;31mYou have no tasks to view.\033[00m\n")
 
-        # TODO update a task
+        # update a task
         elif (user_choice == "4"):
-            print("Updating task")
+            
+            # verify the database holds a task to update
+            if tasks == None:
+                print("\n\033[1;31mYou have no tasks to update!\033[00m\n")
+                continue
 
-        # quit Firebase to do task manager
+            # loop until user select a valid task to update
+            while True:
+
+                # prompt user for which task to update
+                view_tasks(tasks)
+                user_choice = input(
+                    "Which task number do you wish to update? ")
+                
+                # make sure the choice can be cast to integer
+                if user_choice.isdigit():
+
+                    # cast to integer and subtract one to match actual element
+                    task_number = int(user_choice) - 1
+
+                    # verify the input falls within the dictionary length
+                    # if so, call the update task function and break loop
+                    if task_number >= 0 and task_number < len(tasks):
+                        update_task(task_number, tasks)
+                        break 
+
+                # otherwise, alert user to invalid input and loop again               
+                print("\nInvalid selection, input a valid task number.\n")
+
+        # quit Firebase To-Do Task Manager
         elif (user_choice == "5"):
-            print("\nGoodbye. Thanks for using Firebase To-Do Task Manager!\n")
+            print("\nGoodbye. Thanks for using ", end="")
+            print("\033[1;36mFirebase To-Do Task Manager\033[00m!\n")
             return 0
 
 
